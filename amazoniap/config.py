1e9fbda99836f209005b1171d676588f2831a0e2
JoshNinetySeven <joshua.oehms@t-online.de>
2017-08-18 16:43:31 +0200

    Add files via upload

---
+++ amazoniap/config.py
@@ -0,0 +1,9 @@
+def can_build(plat):
+    return plat == 'android'
+
+def configure(env):
+	if env['platform'] == 'android':
+		env.android_add_dependency("compile files('../../../modules/amazoniap/android/lib//in-app-purchasing-2.0.76.jar')")
+		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
+		env.android_add_java_dir("android/src")
+		env.disable_module()
